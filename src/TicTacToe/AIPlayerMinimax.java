8efa36633ff0feb01e8e7bbb41073d1b82dbb876
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:23:23 +0700

    method minimax

--- src/TicTacToe/AIPlayerMinimax.java
+++ src/TicTacToe/AIPlayerMinimax.java
@@ -7,3 +7,62 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+package TicTacToe;
+
+import java.util.*;
+/** AIPlayer using Minimax algorithm */
+public class AIPlayerMinimax extends AIPlayer {
+
+    /** Constructor with the given game board */
+    public AIPlayerMinimax(Board board) {
+        super(board);
+    }
+
+    /** Get next best move for computer. Return int[2] of {row, col} */
+    @Override
+    int[] move() {
+        int[] result = minimax(2, mySeed); // depth, max turn
+        return new int[] {result[1], result[2]};   // row, col
+    }
+
+    /** Recursive minimax at level of depth for either maximizing or minimizing player.
+     Return int[3] of {score, row, col}  */
+    private int[] minimax(int depth, Seed player) {
+        // Generate possible next moves in a List of int[2] of {row, col}.
+        List<int[]> nextMoves = generateMoves();
+
+        // mySeed is maximizing; while oppSeed is minimizing
+        int bestScore = (player == mySeed) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int currentScore;
+        int bestRow = -1;
+        int bestCol = -1;
+
+        if (nextMoves.isEmpty() || depth == 0) {
+            // Gameover or depth reached, evaluate score
+            bestScore = evaluate();
+        } else {
+            for (int[] move : nextMoves) {
+                // Try this move for the current "player"
+                cells[move[0]][move[1]].content = player;
+                if (player == mySeed) {  // mySeed (computer) is maximizing player
+                    currentScore = minimax(depth - 1, oppSeed)[0];
+                    if (currentScore > bestScore) {
+                        bestScore = currentScore;
+                        bestRow = move[0];
+                        bestCol = move[1];
+                    }
+                } else {  // oppSeed is minimizing player
+                    currentScore = minimax(depth - 1, mySeed)[0];
+                    if (currentScore < bestScore) {
+                        bestScore = currentScore;
+                        bestRow = move[0];
+                        bestCol = move[1];
+                    }
+                }
+                // Undo move
+                cells[move[0]][move[1]].content = Seed.NO_SEED;
+            }
+        }
+        return new int[] {bestScore, bestRow, bestCol};
+    }
+}
