a4a2ad066421b0a0c0cba6d252639ed45f1715ee
Nailah Adlina <162094987+adljna@users.noreply.github.com>
2024-12-20 23:42:56 +0700

    Update import

--- src/TicTacToe/MainMenu.java
+++ src/TicTacToe/MainMenu.java
@@ -7,3 +7,22 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+
+package TicTacToe;
+
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class MainMenu extends JPanel {
+    private ImageIcon backgroundImageIcon;  // Use ImageIcon to handle GIF
+    private Image singlePlayerButtonImage;
+    private Image multiplayerButtonImage;
+    private JFrame parentFrame;
+}
