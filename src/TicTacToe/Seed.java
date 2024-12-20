ea93d552a48fd014b9373b242e289740f7b2e24c
cnsalsabila <162676136+cnsalsabila@users.noreply.github.com>
2024-12-20 23:12:40 +0700

    Update Seed.java

--- src/TicTacToe/Seed.java
+++ src/TicTacToe/Seed.java
@@ -1,4 +1,60 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #11
+ * 1 - 5026231069 - Muhammad Zaky Al Khair
+ * 2 - 5026231068 - Nailah Adlina
+ * 3 - 5026231173 - Naura Salsabila
+ */
 package TicTacToe;
 
-public enum Seed {
+import java.awt.Image;
+import java.net.URL;
+import javax.swing.ImageIcon;
+/**
+ * This enum is used by:
+ * 1. Player: takes value of CROSS or NOUGHT
+ * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
+ *
+ * We also attach a display image icon (text or image) for the items.
+ *   and define the related variable/constructor/getter.
+ * To draw the image:
+ *   g.drawImage(content.getImage(), x, y, width, height, null);
+ *
+ * Ideally, we should define two enums with inheritance, which is,
+ *  however, not supported.
+ */
+public enum Seed {   // to save as "Seed.java"
+    CROSS("X", "test.gif"),   // displayName, imageFilename
+    NOUGHT("O", "bismillah.gif"),
+    NO_SEED(" ", null);
+
+    // Private variables
+    private String displayName;
+    private Image img = null;
+
+    // Constructor (must be private)
+    private Seed(String name, String imageFilename) {
+        this.displayName = name;
+
+        if (imageFilename != null) {
+            URL imgURL = getClass().getClassLoader().getResource(imageFilename);
+            ImageIcon icon = null;
+            if (imgURL != null) {
+                icon = new ImageIcon(imgURL);
+                this.img = icon.getImage(); // Ensure the gif will be properly animated
+            } else {
+                System.err.println("Couldn't find file " + imageFilename);
+            }
+        }
+    }
+
+    // Public getters
+    public String getDisplayName() {
+        return displayName;
+    }
+    public Image getImage() {
+        return img;
+    }
 }
