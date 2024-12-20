1a3e4c13b07a2576875cdb23527fd4d7d4d22da1
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:31:37 +0700

    Update stepgame

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -52,3 +52,41 @@ public class Board {
             }
         }
     }
+    /**
+     *  The given player makes a move on (selectedRow, selectedCol).
+     *  Update cells[selectedRow][selectedCol]. Compute and return the
+     *  new game state (PLAYING, DRAW, CROSS_WON, NOUGHT_WON).
+     */
+    public State stepGame(Seed player, int selectedRow, int selectedCol) {
+        // Update game board
+        cells[selectedRow][selectedCol].content = player;
+
+        // Compute and return the new game state
+        if (cells[selectedRow][0].content == player  // 3-in-the-row
+                && cells[selectedRow][1].content == player
+                && cells[selectedRow][2].content == player
+                || cells[0][selectedCol].content == player // 3-in-the-column
+                && cells[1][selectedCol].content == player
+                && cells[2][selectedCol].content == player
+                || selectedRow == selectedCol     // 3-in-the-diagonal
+                && cells[0][0].content == player
+                && cells[1][1].content == player
+                && cells[2][2].content == player
+                || selectedRow + selectedCol == 2 // 3-in-the-opposite-diagonal
+                && cells[0][2].content == player
+                && cells[1][1].content == player
+                && cells[2][0].content == player) {
+            return (player == Seed.CROSS) ? State.CROSS_WON : State.NOUGHT_WON;
+        } else {
+            // Nobody win. Check for DRAW (all cells occupied) or PLAYING.
+            for (int row = 0; row < ROWS; ++row) {
+                for (int col = 0; col < COLS; ++col) {
+                    if (cells[row][col].content == Seed.NO_SEED) {
+                        return State.PLAYING; // still have empty cells
+                    }
+                }
+            }
+            return State.DRAW; // no empty cell, it's a draw
+        }
+    }
+    
