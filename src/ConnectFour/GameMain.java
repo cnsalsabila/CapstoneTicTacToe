0859ac607c25c0eba1ba702d3674e03d6bdf97ed
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:55:25 +0700

    Update import

--- src/ConnectFour/GameMain.java
+++ src/ConnectFour/GameMain.java
@@ -7,3 +7,27 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+package ConnectFour;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class GameMain extends JPanel {
+    private static final long serialVersionUID = 1L; // to prevent serializable warning
+
+    // Define named constants for the drawing graphics
+    public static final String TITLE = "Tic Tac Toe";
+    public static final Color COLOR_BG_START = new Color(240, 248, 255); // Alice Blue
+    public static final Color COLOR_BG_END = new Color(135, 206, 250);   // Light Sky Blue
+    public static final Color COLOR_STATUS_BAR = new Color(255, 255, 255); // Lavender
+    public static final Color COLOR_CROSS = new Color(255, 69, 0);  // Orange Red
+    public static final Color COLOR_NOUGHT = new Color(0, 191, 255); // Deep Sky Blue
+    public static final Font FONT_STATUS = new Font("Arial", Font.BOLD, 16);
+
+    // Define game objects
+    private Board board;         // the game board
+    private State currentState;  // the current state of the game
+    private Seed currentPlayer;  // the current player
+    private JLabel statusBar;    // for displaying status message
+
+    /** Constructor to setup the UI and game components */
