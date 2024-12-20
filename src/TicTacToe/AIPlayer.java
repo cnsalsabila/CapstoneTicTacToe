3280ff685814fe4dab7100271e5ce5ae079eb3aa
Nailah Adlina <162094987+adljna@users.noreply.github.com>
2024-12-20 23:21:16 +0700

    constructor

--- src/TicTacToe/AIPlayer.java
+++ src/TicTacToe/AIPlayer.java
@@ -7,3 +7,11 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+
+public abstract class AIPlayer {
+    protected int ROWS = Board.ROWS;  // number of rows
+    protected int COLS = Board.COLS;  // number of columns
+
+    protected Cell[][] cells; // the board's ROWS-by-COLS array of Cells
+    protected Seed mySeed;    // computer's seed
+    protected Seed oppSeed;   // opponent's seed
