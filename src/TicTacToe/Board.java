0ea3b0926937f3e563e27708505c3f0d17012191
cnsalsabila <162676136+cnsalsabila@users.noreply.github.com>
2024-12-20 23:27:30 +0700

    Update Board.java

--- src/TicTacToe/Board.java
+++ src/TicTacToe/Board.java
@@ -7,3 +7,48 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+package TicTacToe;
+import java.awt.*;
+/**
+ * The Board class models the ROWS-by-COLS game board.
+ */
+public class Board {
+    // Define named constants
+    public static final int ROWS = 3;  // ROWS x COLS cells
+    public static final int COLS = 3;
+    // Define named constants for drawing
+    public static final int CANVAS_WIDTH = Cell.SIZE * COLS;  // the drawing canvas
+    public static final int CANVAS_HEIGHT = Cell.SIZE * ROWS;
+    public static final int GRID_WIDTH = 8;  // Grid-line's width
+    public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2; // Grid-line's half-width
+    public static final Color COLOR_GRID = Color.WHITE;  // Change grid lines to white
+    public static final int Y_OFFSET = 1;  // Fine tune for better display
+
+    // Define properties (package-visible)
+    /** Composes of 2D array of ROWS-by-COLS Cell instances */
+    Cell[][] cells;
+
+    /** Constructor to initialize the game board */
+    public Board() {
+        initGame();
+    }
+
+    /** Initialize the game objects (run once) */
+    public void initGame() {
+        cells = new Cell[ROWS][COLS];
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                cells[row][col] = new Cell(row, col);
+                cells[row][col].content = Seed.NO_SEED; // Ensure cells are empty
+            }
+        }
+    }
+
+    /** Reset the game board, ready for new game */
+    public void newGame() {
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                cells[row][col].newGame(); // clear the cell content
+            }
+        }
+    }
