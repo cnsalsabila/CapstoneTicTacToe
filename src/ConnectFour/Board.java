f5ee451872a588943aeddd9c57ae8e2a93a2afe3
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:53:44 +0700

    Update import

--- src/ConnectFour/Board.java
+++ src/ConnectFour/Board.java
@@ -7,3 +7,29 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+package ConnectFour;
+import java.awt.*;
+/**
+ * The Board class models the ROWS-by-COLS game board.
+ */
+public class Board {
+    // Define named constants
+    public static final int ROWS = 6;  // ROWS x COLS cells
+    public static final int COLS = 7;
+    // Define named constants for drawing
+    public static final int CANVAS_WIDTH = Cell.SIZE * COLS;  // the drawing canvas
+    public static final int CANVAS_HEIGHT = Cell.SIZE * ROWS;
+    public static final int GRID_WIDTH = 8;  // Grid-line's width
+    public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2; // Grid-line's half-width
+    public static final Color COLOR_GRID = Color.WHITE;  // Change grid lines to white
+    public static final int Y_OFFSET = 1;  // Fine tune for better display
+
+    // Define properties (package-visible)
+    /** Composes of 2D array of ROWS-by-COLS Cell instances */
+    Cell[][] cells;
+
+    /** Constructor to initialize the game board */
+    public Board() {
+        initGame();
+    }
+}
