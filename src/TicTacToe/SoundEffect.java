c5da430be3d2a2e2a71091be73b1112ccbb0be91
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:45:57 +0700

    Update SoundEffect.java

--- src/TicTacToe/SoundEffect.java
+++ src/TicTacToe/SoundEffect.java
@@ -7,3 +7,29 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+ackage TicTacToe;
+
+import java.io.IOException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public enum SoundEffect {
+    GAME_START("TicTacToe/bgmusic.wav", true), // Background music (looping)
+    GAME_WON("TicTacToe/win.wav", false),      // Winning sound
+    GAME_LOST("TicTacToe/lose.wav", false),    // Losing sound
+    GAME_DRAW("TicTacToe/seri.wav", false);    // Draw sound
+
+    public static enum Volume {
+        MUTE, LOW, MEDIUM, HIGH
+    }
+
+    public static Volume volume = Volume.LOW;
+
+    public Clip clip;
+    private boolean shouldLoop; // Flag to determine if this sound should loop
+
+}
