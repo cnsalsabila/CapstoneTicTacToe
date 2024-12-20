6f593158aed063f3cdee7c662540dfe55125319e
cnsalsabila <162676136+cnsalsabila@users.noreply.github.com>
2024-12-20 23:47:33 +0700

    Update State.java

--- src/TicTacToe/State.java
+++ src/TicTacToe/State.java
@@ -7,3 +7,11 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+package TicTacToe;
+
+/**
+ *  The enum State contains the various game states of the TTT game
+ */
+public enum State {  // to save as "State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
