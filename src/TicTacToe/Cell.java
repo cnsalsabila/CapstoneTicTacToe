1298dd2685b5b83740aea5e4149c79f627c8ab88
Nailah Adlina <162094987+adljna@users.noreply.github.com>
2024-12-20 23:33:53 +0700

    Update Cell

--- src/TicTacToe/Cell.java
+++ src/TicTacToe/Cell.java
@@ -7,3 +7,27 @@
  * 2 - 5026231068 - Nailah Adlina
  * 3 - 5026231173 - Naura Salsabila
  */
+
+package TicTacToe;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * The Cell class models each individual cell of the game board.
+ */
+public class Cell {
+    // Define named constants for drawing
+    public static final int SIZE = 200; // cell width/height (square)
+    // Symbols (cross/nought) are displayed inside a cell, with padding from border
+    public static final int PADDING = SIZE / 10;
+    public static final int SEED_SIZE = SIZE - PADDING * 2;
+
+    // Define properties (package-visible)
+    /** Content of this cell (Seed.EMPTY, Seed.CROSS, or Seed.NOUGHT) */
+    Seed content;
+    /** Row and column of this cell */
+    int row, col;
+    private Image backgroundImage;  // To store the background image
+}
