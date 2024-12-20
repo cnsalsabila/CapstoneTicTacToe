4733353a91751a96ee25f8af3a7c3dc5a45eab4e
zakyalkhair <162418824+zakyalkhair@users.noreply.github.com>
2024-12-20 23:52:28 +0700

    Update header

--- src/ConnectFour/Seed.java
+++ src/ConnectFour/Seed.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #11
+ * 1 - 5026231069 - Muhammad Zaky Al Khair
+ * 2 - 5026231068 - Nailah Adlina
+ * 3 - 5026231173 - Naura Salsabila
+ */
 package ConnectFour;
 
 public enum Seed {
